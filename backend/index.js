be6d6ed223cfe2f2cb330c5c91e08e4e7d1987da
Anuj2117 <anujkushwaha221@gmail.com>
2024-12-19 23:15:33 +0530

    add backend url

--- backend/index.js
+++ backend/index.js
@@ -13,9 +13,15 @@ dotenv.config();
 // middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    origin:"http://localhost:5173",
-}));
+app.use(
+    cors({
+      origin:
+        "https://chattify-2-git-main-anuj-kushwahas-projects-db7b729e.vercel.app",
+      credentials: true,
+      methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+      allowedHeaders: ["content-Type", "Authorization"],
+    })
+  );
 
 const PORT = process.env.PORT || 3001;
 const URI = process.env.MONGODB_URI;
