f3a4998d30b72f304e1d54e6c8999c4520a77f75
Anuj2117 <anujkushwaha221@gmail.com>
2024-12-19 23:28:27 +0530

    chnage on url

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -21,7 +21,7 @@ function Login() {
     };
 
     axios
-      .post("/api/user/login", userInfo)
+      .post("https://chattify-2.onrender.com/api/user/login", userInfo)
       .then((response) => {
         if (response.data) {
           toast.success("Login successful");
